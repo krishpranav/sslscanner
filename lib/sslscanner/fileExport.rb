b8a2dfb12e74106e3f4295f0df0c1ba1bfbdc4fb
krishpranav <krisna.pranav@gmail.com>
2021-05-26 15:00:27 +0530

    [+] txt file save [+]

--- lib/sslscanner/fileExport.rb
+++ lib/sslscanner/fileExport.rb
@@ -5,4 +5,10 @@ module ScanSSL
                 text "File saved as #{data}"
             end
         end
+
+        def self.txt(file, data)
+            ftxt = File.open("#{path}/#{file}", "a")
+            ftxt.write(data)
+            ftxt.close
+        end
         
