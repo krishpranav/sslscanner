9ba88f137555e0b1269080d898a249dd2018cbf5
krishpranav <krisna.pranav@gmail.com>
2021-05-26 13:26:17 +0530

    [+] Added some functions for ssl scanning [+]

--- lib/sslscanner.rb
+++ lib/sslscanner.rb
@@ -13,3 +13,23 @@ require 'sslscanner/settings'
 require 'sslscanner/certInfo'
 require 'sslscanner/fileExport'
 require 'sslscanner/scanHost'
+
+module ScanSSL
+    class Command
+        def self.call(options = {})
+            @server = options[:server]
+            @port = options[:port]
+
+            if options[:check_cert] == true
+                a = ScanSSL::CertInfo.new(@server, @port)
+                colorOutputCert(a.valid?,
+                    a.valid_from, 
+                    a.valid_until, 
+                    a.issuer, 
+                    a.subject, 
+                    a.algorithm, 
+                    a.key_size, 
+                    a.public_key)
+                end
+
+                
