b742889568d71f8bf7d6ee32b8f4349c5160a2e2
krishpranav <krisna.pranav@gmail.com>
2021-05-26 13:32:39 +0530

    [+] Added some initialize functions for connections [+]

---
+++ lib/sslscanner/certInfo.rb
@@ -0,0 +1,11 @@
+module ScanSSL
+    class CertInfo < Certificate 
+        def initialize(server, port)
+            @ssl_context = OpenSSL::SSL::SSLContext.new
+            @cert_store = OpenSSL::X509::Store.new
+            @cert_store.set_default_paths
+            @ssl_context.cert_store = @cert_store
+            @tcp_socket = TCPSocket.new(server, port)
+            @socket_destination = OpenSSL::SSL::SSLSocket.new @tcp_socket, @ssl_context
+            @socket_destination.connect
+        end
